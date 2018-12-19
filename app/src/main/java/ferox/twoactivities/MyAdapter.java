89f1f7a70a8feb7251445f28a6856d43cd01b0b0
Adrian Brooks <brooksat@email.sc.edu>
2018-12-18 21:36:27 -0500

    Made framework for a view that displays a round, may or may not work as it is part of a list where as dynamically adding matchviews to a linear layout may be better. Need to take into consideration if I will be able to make spaces in the case of an uneven bracket and if I will be able to draw the lines connecting the matches of one round to the next.

---
+++ app/src/main/java/ferox/twoactivities/MyAdapter.java
@@ -0,0 +1,57 @@
+package ferox.twoactivities;
+
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+
+    public static class  MyViewHolder extends  RecyclerView.ViewHolder {
+        public ConstraintLayout mMatchView;
+        public MyViewHolder(ConstraintLayout v){
+            super(v);
+            mMatchView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(String[] myDataset) {
+        mDataset = myDataset;
+    }
+
+    //Create new views(invoked by teh layout manager)
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
+        //create new view
+        ConstraintLayout match = (ConstraintLayout) LayoutInflater.from(parent.getContext()).inflate(R.layout.match, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(match);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        //holder.mTextView.setText(mDataset[position]);
+
+    }
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+
+
+
+
+}
--- app/src/main/java/ferox/twoactivities/bracket.java
+++ app/src/main/java/ferox/twoactivities/bracket.java
@@ -4,18 +4,36 @@ import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.text.Layout;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class bracket extends AppCompatActivity {
 
+    private RecyclerView mRecyclerView;
+    private  RecyclerView.Adapter mAdapter;
+    private  RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataSet = {"1","", ""};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bracket);
+        mRecyclerView = findViewById(R.id.round);
 
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+       // mRecyclerView.setHasFixedSize(true);
 
+        //use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        //specify an adapter
+        mAdapter = new MyAdapter(myDataSet);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
 
