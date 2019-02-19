79a6bdf26a716d22baa5874eb77e2d5be2f36c75
Adrian Brooks <brooksat@email.sc.edu>
2019-02-19 13:13:39 -0500

    Set up tabs on bracket page

---
+++ app/src/main/java/ferox/bracket/ViewPagerNoHorsScroll.java
@@ -0,0 +1,33 @@
+package ferox.bracket;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+
+public class ViewPagerNoHorsScroll extends ViewPager {
+
+
+    public ViewPagerNoHorsScroll(@NonNull Context context) {
+        super(context);
+    }
+
+    public ViewPagerNoHorsScroll(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+
+}
